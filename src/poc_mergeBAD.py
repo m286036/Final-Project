d733b27dd4df891d47179068a86c1c4c2eaa348c
Zack Goforth <m282346@usna.edu>
2026-04-16 15:23:36 -0400

    renamed the original merge file

---
+++ src/poc_mergeBAD.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+
+energy=pd.read_csv("Final-Project/raw/electricityYearly.csv")
+usa=pd.read_csv("Final-Poject/raw/primeContracts.csv")
+seds = pd.read_csv("Final-Project/raw/seds.csv", low_memory=False)
+
+
+##I dont know about you guys but I cannot figure out how to make these csvs show up in my terminal.
+###It may require Pathlib, but I would ask Dr Roche during class how to make it so we can see these csvs, if I can't
+##figure it out on the bus
+
+
+usa = usa[(usa["awarding_agency_name"].str.contains("Department of Energy", case=False, na=False)) |(usa["funding_agency_name"].str.contains("Department of Energy", case=False, na=False))]
+##gemini gave us these functions to filter down the rows and columns in the usa dataset that we need.
+usa = usa[["action_date_fiscal_year","federal_action_obligation","primary_place_of_performance_state_code","primary_place_of_performance_state_name"]]
+
+##gemini explained dropna function to drop the rows/columns that have Na in them
+usa = usa.dropna(subset=["state_code", "state", "year", "doe_contract_dollars"])
+usa["year"] = usa["year"].astype(int)
+
+
+grouped_usa_df = usa.groupby(["state_code", "state", "year"], as_index=False)["doe_contract_dollars"].sum()
+##This function groups the usa data by the state code and year, adding the dollars as a running total.
+
+
+
+renew = energy[(energy["Category"] == "Electricity generation") &(energy["Variable"] == "Renewables") &(energy["Unit"] == "GWh")]
+
+print(grouped_usa_df)
+print(renew)
