29736401a4c3b0bd71e00eab57c538cc1d3188ac
m286036 <m286036@usna.edu>
2026-04-22 09:11:29 -0400

    Graph code

--- data/clean/rdr.py
+++ data/clean/rdr.py
@@ -3,9 +3,9 @@ import pandas as pd
 df=pd.read_csv("merged.csv", sep="\t")
 early_df = df[df['Year']==2017]
 Wyoming_df=df[df["state"]=="Wyoming"]
-California_df=df[df["state"]=="California"]
+California_df=df[df["state"]=="California"][df['Year'] != 2015][df['Year'] != 2016]
 late_df=df[df['Year']==2023]
-print(early_df)
+##print(early_df)
 
 import plotly.express as px
 from plotly.colors import sequential as colors #got this from SD211 covid lab
@@ -47,4 +47,50 @@ Cal_fig = px.line(California_df,
               color="state",
               title="Change in DOE funding to California (2017-2023)")
 
-fig.show()
+Cal_fig.show()
+
+
+Flo_df=df[df["state"]=="Florida"]
+Flo_fig = px.line(Flo_df, 
+              x= "Year", 
+              y="doe_funding_dollars", 
+              color="state",
+              title="Change in DOE funding to Florida (2017-2023)")
+
+Flo_fig2 = px.line(Flo_df, 
+              x= "Year", 
+              y="Data", 
+              color="state",
+              title="Change in Renewable Energy Generation in Florida (2015-2023)")
+
+#Flo_fig2.show()
+
+Col_df=df[df["state"]=="Colorado"][df['Year'] != 2016]
+Col_fig = px.line(Col_df, 
+              x= "Year", 
+              y="doe_funding_dollars", 
+              color="state",
+              title="Change in DOE funding to Colorado (2017-2023)")
+
+Col_fig2 = px.line(Col_df, 
+              x= "Year", 
+              y="Data", 
+              color="state",
+              title="Change in Renewable Energy Generation in Colorado (2015-2023)")
+
+##Col_fig.show()
+
+Ken_df=df[df["state"]=="Kentucky"][df['Year'] != 2023]
+Ken_fig = px.line(Ken_df, 
+              x= "Year", 
+              y="doe_funding_dollars", 
+              color="state",
+              title="Change in DOE funding to Kentucky (2017-2023)")
+
+Ken_fig2 = px.line(Ken_df, 
+              x= "Year", 
+              y="Data", 
+              color="state",
+              title="Change in Renewable Energy Generation in Kentucky (2015-2023)")
+
+#Ken_fig2.show()
